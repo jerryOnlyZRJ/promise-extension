dad11dd5aa8e78e90a2ca12f69c0d85e528d053f
ranjayzheng <ranjayzheng@qq.com>
2018-11-17 11:16:39 +0800

    update eslint rules

--- README.md
+++ README.md
@@ -6,4 +6,65 @@ We add some common methods in Promise class.
 
 ## Install
 
-``
+`$npm install --save-dev promise-extension-api`
+
+## Usage
+
+```js
+require('promise-extension-api')
+
+Promise.step([...PromiseCreators])
+```
+
+### Promise.step
+
+Resolve promises in order.
+
+```js
+const timeout = ms => {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms)
+    })
+}
+
+const ajax1 = () => {
+    return timeout(200).then(() => {
+        console.log(1)
+        return 1
+    })
+}
+
+const ajax2 = () => {
+    return timeout(100).then(() => {
+        console.log(2)
+        return 2
+    })
+}
+
+const ajax3 = () => {
+    return timeout(200).then(() => {
+        console.log(3)
+        return 3
+    })
+}
+
+//It will console
+//1
+//2
+//3
+//done
+//[1,2,3]
+Promise.step([ajax1, ajax2, ajax3]).then(data => {
+    console.log("done")
+    console.log(data)
+    return data
+})
+```
+
+## License
+
+MIT
+
+## Keywords
+
+Promise
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,47 +1,47 @@
 require('../index.js')
 
 describe('test index.js', () => {
-    test('test Promise.step', async () => {
-        const timeout = ms => {
-            return new Promise((resolve, reject) => {
-                setTimeout(resolve, ms)
-            })
-        }
+  test('test Promise.step', async () => {
+    const timeout = ms => {
+      return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms)
+      })
+    }
 
-        const ajax1 = () => {
-            return timeout(200).then(() => {
-                console.log(1)
-                return 1
-            })
-        }
+    const ajax1 = () => {
+      return timeout(200).then(() => {
+        console.log(1)
+        return 1
+      })
+    }
 
-        const ajax2 = () => {
-            return timeout(100).then(() => {
-                console.log(2)
-                return 2
-            })
-        }
+    const ajax2 = () => {
+      return timeout(100).then(() => {
+        console.log(2)
+        return 2
+      })
+    }
 
-        const ajax3 = () => {
-            return timeout(200).then(() => {
-                console.log(3)
-                return 3
-            })
-        }
+    const ajax3 = () => {
+      return timeout(200).then(() => {
+        console.log(3)
+        return 3
+      })
+    }
 
-        const result = await Promise.step([ajax1, ajax2, ajax3]).then(data => {
-            console.log("done")
-            return data
-        })
-
-        expect(JSON.stringify(result)).toBe("[1,2,3]")
+    const result = await Promise.step([ajax1, ajax2, ajax3]).then(data => {
+      console.log('done')
+      return data
     })
 
-    test('Promise.step unexpect input', () => {
-        try {
-            Promise.step(123)
-        }catch(e){
-            expect(e).not.toBeNull()
-        }
-    })
+    expect(JSON.stringify(result)).toBe('[1,2,3]')
+  })
+
+  test('Promise.step unexpect input', () => {
+    try {
+      Promise.step(123)
+    } catch (e) {
+      expect(e).not.toBeNull()
+    }
+  })
 })
